563f4029bd4457002d7bd0013789fac8d2e1adec
konradfus <konrad.fus@ttms.pl>
2016-03-30 07:49:21 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,20 +4,18 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-
-get "/" do
-  erb :home
-end
-
-
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
+
+get "/" do
+  erb :home
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
